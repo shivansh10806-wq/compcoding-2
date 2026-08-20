a51c52996380ab6c0d5c1ceeeb498707cbf86bae
shivansh10806-wq <shivansh10806@gmail.com>
2026-08-20 10:05:20 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (81.60%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+         int maxSum = nums[0];
+        int currentSum = 0;
+        for (int num : nums) {
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+             currentSum += num;
+            maxSum = max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+};
